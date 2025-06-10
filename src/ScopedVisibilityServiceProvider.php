ad2781701f0e35b33e2486e7e9f991112dc638fc
simon <sdiaz.sz@gmail.com>
2025-06-10 04:43:26 -0400

    first

---
+++ src/ScopedVisibilityServiceProvider.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Cosmoscript\ScopedVisibility;
+
+use Larangular\Installable\Support\InstallableServiceProvider as ServiceProvider;
+
+class ScopedVisibilityServiceProvider extends ServiceProvider {
+
+    protected $defer = false;
+
+    public function boot(): void {
+    }
+
+}
