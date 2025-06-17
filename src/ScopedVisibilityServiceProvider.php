ff22817b9fd8f51e7f22e82715b36592955dfd32
Simon <2222031+sdiazsaez@users.noreply.github.com>
2025-06-16 21:19:18 -0400

    Update ScopedVisibilityServiceProvider.php

--- src/ScopedVisibilityServiceProvider.php
+++ src/ScopedVisibilityServiceProvider.php
@@ -10,29 +10,6 @@ class ScopedVisibilityServiceProvider extends ServiceProvider {
     protected $defer = false;
 
     public function boot(): void {
-        
-        Builder::macro('applyVisibility', function (array $flags = []) {
-            if (! method_exists($this->getModel(), 'scopedFlags')) {
-                return $this;
-            }
-        
-            foreach ($flags as $key => $mode) {
-                $mode = strtolower($mode);
-        
-                if (!in_array($mode, ['only', 'with'])) {
-                    continue;
-                }
-        
-                $method = $mode === 'only' ? 'onlyScope' : 'withScope';
-        
-                if (array_key_exists($key, $this->getModel()->scopedFlags())) {
-                    $this->$method($key);
-                }
-            }
-        
-            return $this;
-        });
-
     }
 
 }
